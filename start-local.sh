05214a07492ec3d1e1c0d080616ee2123ad6f1cf
Tesso Martins Costa a.k.a CodeMonkeyBR <tesso.martins@gmail.com>
2018-06-24 18:00:46 -0300

    fix

--- start-local.sh
+++ start-local.sh
@@ -3,7 +3,7 @@
 docker run -it --rm -v "$(pwd)":/opt/maven -w /opt/maven maven:3.5.0-jdk-8 mvn -Dmaven.repo.local=/opt/maven/.m2_docker clean install -DskipTests
 
 if [[ "$uname" == 'Linux' ]]; then
-   sudo chown $(whoami) -R just-food-*/target
+   sudo chown $(whoami) -R skip-*/target
 fi
 
 docker-compose -f docker-compose-local.yml up --build
